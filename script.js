905bcc0ef980a7df5d3d5084e4744ee7a4fe2932
Himanshu Kashyap <38201652+himanshuxgit@users.noreply.github.com>
2023-08-05 13:54:18 +0530

    added other required buttons

--- script.js
+++ script.js
@@ -43,3 +43,59 @@ class Book {
     card.appendChild(title);
     card.appendChild(author);
     card.appendChild(pages);
+    const readBtn = document.createElement('button');
+    readBtn.className = 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded';
+    readBtn.textContent = book.read ? 'Read' : 'Not Read';
+    readBtn.addEventListener('click', () => {
+      book.read = !book.read;
+      displayBooks();
+    });
+    card.appendChild(readBtn);
+  
+    const removeBtn = document.createElement('button');
+    removeBtn.className = 'bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-4';
+    removeBtn.textContent = 'Remove';
+    removeBtn.addEventListener('click', () => {
+      myLibrary.splice(myLibrary.indexOf(book), 1);
+      displayBooks();
+    });
+    card.appendChild(removeBtn);
+  
+    return card;
+  }
+  
+  const newBookForm = document.getElementById('new-book-form');
+  const popup = document.getElementById('popup');
+  
+  const newBookBtn = document.getElementById('new-book');
+  newBookBtn.addEventListener('click', () => {
+    popup.classList.remove('hidden');
+  });
+  
+  window.addEventListener('click', (e) => {
+    if (e.target === popup) {
+      popup.classList.add('hidden');
+      newBookForm.reset();
+    }
+  });
+  
+  newBookForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = parseInt(document.getElementById('pages').value);
+    const read = document.getElementById('read').checked;
+  
+    const book = new Book(title, author, pages, read);
+    addBookToLibrary(book);
+  
+    newBookForm.reset();
+    popup.classList.add('hidden');
+  
+    displayBooks();
+  });
+  
+  addBookToLibrary(new Book('The Hobbit', 'JRR Tolkien', 295, true));
+  addBookToLibrary(new Book('Harry Potter', 'JK Rowling', 500, false));
+  
+  displayBooks();
